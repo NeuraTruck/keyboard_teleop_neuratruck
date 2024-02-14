113535095a1f648f4df7170e79f7c82343097cac
tstaisyu <t_shiba117@yahoo.co.jp>
2024-02-14 20:16:06 +0900

    Update

--- launch/keyboard_teleop.launch.py
+++ launch/keyboard_teleop.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
     gzclient_launch_path = os.path.join(gazebo_ros_share_path, 'launch', 'gzclient.launch.py')
 
     # ワールドファイルのパスを設定
-    world_file_path = LaunchConfiguration('world', default=os.path.join(gazebo_ros_share_path, 'worlds', 'empty.world'))
+    world_file_path = LaunchConfiguration('world', default=os.path.join(get_package_share_directory('your_package_name'), 'worlds', 'your_world_file.world'))
 
     # GAZEBOのサーバーとクライアントを起動するlaunchファイルを含む
     gzserver = IncludeLaunchDescription(
@@ -26,6 +26,11 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource(gzclient_launch_path),
             )
 
+    urdf_file_path = os.path.join(get_package_share_directory('truckmodel'), 'sdf', 'model.sdf')
+
+    with open(urdf_file_path, 'r') as urdf_file:
+        robot_description = urdf_file.read()
+        
     # 自律搬送車両モデルを配置するノード
     spawn_entity = Node(
         package='gazebo_ros', executable='spawn_entity.py',
